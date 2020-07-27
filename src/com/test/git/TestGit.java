8954d88907df5cb2e6f12387834bc6d5479ca139
Owner <Owner@DESKTOP-C42QD54.fios-router.home>
2020-07-27 17:39:51 -0400

    Dev branch added

--- src/com/test/git/TestGit.java
+++ src/com/test/git/TestGit.java
@@ -5,5 +5,6 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("created new class to test git pull");
 		System.out.println("new change");
+		System.out.println("DEV branch switched and added line");
 	}
 }
